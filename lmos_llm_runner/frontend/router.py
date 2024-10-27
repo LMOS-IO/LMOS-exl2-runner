3a5fecb4c8bf0b19021636d16a4d9498491bdff7
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2024-10-27 18:38:19 +0000

    add openai types

--- lmos_llm_runner/frontend/router.py
+++ lmos_llm_runner/frontend/router.py
@@ -1,18 +1,20 @@
 from fastapi import APIRouter
-from zmq_server import zmq_producer
+from .zmq_server import zmq_producer
+
+from lmos_openai_types import CreateChatCompletionRequest, CreateCompletionRequest
 
 router = APIRouter(prefix="/v1", tags=["openAI"])
 
 
 @router.post("/completions")
-async def generate_completion():
+async def generate_completion(data: CreateCompletionRequest):
     """Function to generate a completion"""
     # TODO: dispatch to ZMQ bridge
     ...
 
 
 @router.post("/chat/completions")
-async def generate_chat_completion():
+async def generate_chat_completion(data: CreateChatCompletionRequest):
     """function to generate a chat completion"""
     # TODO: dispatch to ZMQ bridge
     ...
